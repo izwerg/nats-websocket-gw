b858b1d87ae657933200335cb3fb40e21a549cb0
Andrew Trubchanin <andrew.trubchanin@gmail.com>
2018-05-24 19:57:48 +0300

    Add -nats-addr, -ws-addr, -ws-route arguments

--- cmd/nats-websocket-gw/main.go
+++ cmd/nats-websocket-gw/main.go
@@ -1,45 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
-	"os"
-
 	"github.com/gorilla/websocket"
-	"github.com/orus-io/nats-websocket-gw"
+	"github.com/izwerg/nats-websocket-gw"
 )
 
-func usage() {
-	fmt.Printf(`Usage: %s [ --help ] [ --no-origin-check ] [ --trace ]
-`, os.Args[0])
-}
-
 func main() {
+	var noOriginCheck, trace bool
+	var natsAddr, wsAddr, wsRoute string
+
+	flag.StringVar(&natsAddr, "nats-addr", "localhost:4222", "host:port of NATS server")
+	flag.StringVar(&wsAddr, "ws-addr", "0.0.0.0:8910", "host:port for WebSockets")
+	flag.StringVar(&wsRoute, "ws-route", "/nats", "route for WebSockets")
+	flag.BoolVar(&noOriginCheck, "no-origin-check", false, "no origin check for WebSockets")
+	flag.BoolVar(&trace, "trace", false, "enable tracing")
+
+	flag.Parse()
+
 	settings := gw.Settings{
-		NatsAddr: "localhost:4222",
+		NatsAddr: natsAddr,
+		Trace:    trace,
 	}
 
-	for _, arg := range os.Args[1:] {
-		switch arg {
-		case "--help":
-			usage()
-			return
-		case "--no-origin-check":
-			settings.WSUpgrader = &websocket.Upgrader{
-				ReadBufferSize:  1024,
-				WriteBufferSize: 1024,
-				CheckOrigin:     func(r *http.Request) bool { return true },
-			}
-		case "--trace":
-			settings.Trace = true
-		default:
-			fmt.Printf("Invalid args: %s\n\n", arg)
-			usage()
-			return
+	if noOriginCheck {
+		settings.WSUpgrader = &websocket.Upgrader{
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+			CheckOrigin:     func(r *http.Request) bool { return true },
 		}
 	}
 
 	gateway := gw.NewGateway(settings)
-	http.HandleFunc("/nats", gateway.Handler)
-	http.ListenAndServe("0.0.0.0:8910", nil)
+	http.HandleFunc(wsRoute, gateway.Handler)
+	fmt.Println(http.ListenAndServe(wsAddr, nil))
 }
